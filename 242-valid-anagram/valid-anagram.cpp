e4b65faa1a99994a94f1648f36b1fc1712c77662
Princy7890 <princyarora65@gmail.com>
2026-06-26 17:34:31 +0530

    Time: 3 ms (46.15%) | Memory: 9.9 MB (25.59%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())
+        {
+            return 0;
+        }
+        unordered_map<char,int>m;
+        for(int i=0;i<s.size();i++)
+        {
+            m[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++)
+        {
+          m[t[i]]--;
+        }
+        for(auto it:m)
+        {
+            if(it.second!=0)
+            {
+                return 0;
+            }
+        }
+        return 1;
+
+
+
+
+
+
+
+    }
+};
